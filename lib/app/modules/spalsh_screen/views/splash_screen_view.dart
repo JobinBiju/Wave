aa023c856d566119700c0d272a22beac2c026977
Jobin Biju <jobinbiju9090@gmail.com>
2021-03-12 15:23:12 +0530

    Splash screen logo changed

--- lib/app/modules/spalsh_screen/views/splash_screen_view.dart
+++ lib/app/modules/spalsh_screen/views/splash_screen_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import 'package:get/get.dart';
 //import 'package:wave/app/theme/color_theme.dart';
@@ -15,12 +16,22 @@ class SplashScreenView extends GetView<SplashScreenController> {
           return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: Get.width * 0.4,
-                width: Get.width * 0.4,
-                child: Image(image: AssetImage('assets/appIcon.png')),
-                decoration: BoxDecoration(
-                  boxShadow: [kDefaultShadow],
+              Neumorphic(
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.concave,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(20)),
+                  depth: 20,
+                  lightSource: LightSource.topLeft,
+                  shadowLightColor: Colors.white30,
+                  shadowDarkColor: Colors.black54,
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  height: Get.width * 0.4,
+                  width: Get.width * 0.4,
+                  child: Image(image: AssetImage('assets/appIcon.png')),
                 ),
               ),
               SizedBox(
