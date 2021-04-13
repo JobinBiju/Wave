83099b05ca3f2a829e54c377264745d6f45fb671
Jobin Biju <jobinbiju9090@gmail.com>
2021-04-13 07:02:43 +0530

    Files restructured

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,7 @@ import 'package:get/get.dart';
 
 import 'package:wave/app/modules/home/bindings/home_binding.dart';
 import 'package:wave/app/modules/home/views/home_view.dart';
-import 'package:wave/app/modules/music_player/bindings/music_player_binding.dart';
-import 'package:wave/app/modules/music_player/views/music_player_view.dart';
+import 'package:wave/app/modules/home/views/music_player_view.dart';
 import 'package:wave/app/modules/spalsh_screen/bindings/splash_screen_binding.dart';
 import 'package:wave/app/modules/spalsh_screen/views/splash_screen_view.dart';
 
@@ -22,11 +21,6 @@ class AppPages {
       name: _Paths.SPALSH_SCREEN,
       page: () => SplashScreenView(),
       binding: SpalshScreenBinding(),
-    ),
-    GetPage(
-      name: _Paths.MUSIC_PLAYER,
-      page: () => MusicPlayerView(),
-      binding: MusicPlayerBinding(),
     ),
   ];
 }
